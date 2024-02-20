5dd722ea14fae094a251f1134d33f0357cb10a9e
Your Future Boyfriend <N1ghtpe0ple@protonmail.com>
2024-02-20 13:25:53 +0700

    Update

--- README.md
+++ README.md
@@ -1,40 +1,28 @@
-# LKx21 - search engine & bypass shorturl
+<div align="center">
 
-![LK21](images/Logo.png)
+# LK21.dl link scraper
 
-## How to Install
-1. Install all dependencies with this command :
-   ```bash
-   sudo apt update
-   sudo apt install git python3 python3-pip
-   pip3 install requests bs4
-   ```
-   [![asciicast](https://asciinema.org/a/332308.png)](https://asciinema.org/a/332308)
-2. Clone this repository by typing :
-   ```bash
-   git clone https://github.com/N1ght420/LKx21
-   ```
-   [![asciicast](https://asciinema.org/a/332310.png)](https://asciinema.org/a/332310)
+</div>
 
-## How to Use
-1. Open LKx21 folder by typing :
-   ```bash
-   cd LKx21
-   ```
-   Notes :
-   > If you clone this repository on $HOME (~/) directory, you can use this command :
-   ```bash
-   cd ~/LKx21
-   ```
-2. Run this Script by typing :
-   ```bash
-   python3 dl.py
-   ```
-3. Input specific title of Movies you want to download :
-   
-   Example :
-   > terminator
+> [!TIP]
+> Open issue if you found any bugs and contribute if you want to add more bugs
 
-   > terminator dark fate
+## Options
 
-   [![asciicast](https://asciinema.org/a/332312.png)](https://asciinema.org/a/332312)
+| Options | Name  | Description  |
+| :-----: | ----- | ------------ |
+| -t      | Title | Movie title  |
+| -l      | Limit | Search limit |
+
+
+## Usage
+```
+python dl.py -t <title>
+```
+**Example :**
+```
+python dl.py -t sherlock
+```
+```
+python dl.py -t "Incantation 2022" -l 2
+```
--- dl.py
+++ dl.py
@@ -1,82 +1,87 @@
-# -*- coding: utf-8 -*-
+import requests, argparse, os
+from prettytable import PrettyTable as PT
+from bs4 import BeautifulSoup as BS
+from selenium import webdriver as WD
+from selenium.webdriver.chrome.service import Service as CS
+from selenium.webdriver.chrome.options import Options as CO
 
-import requests, os, argparse, re
-from bs4 import BeautifulSoup
+options = CO()
+opts = ["headless", "disable-dev-shm-usage", "no-sandbox", "disable-gpu", "enable-javascript", "user-agent"]
+for opt in opts: options.add_argument(f"--{opt}")
 
 parse = argparse.ArgumentParser()
 parse.add_argument('-t', help='Judul Film', dest='title')
-parse.add_argument('-r', help='Resolusi', dest='reso')
+parse.add_argument('-l', help="Limit", dest='limit')
 args = parse.parse_args()
 
-judul = args.title
-areso = args.reso
+title = input("Masukkan judul : ") if not args.title else args.title
+res = requests.get("https://amp.lk21official.mom/search.php", params={"s": title}, headers={"User-Agent": "Mozilla/5.0"})
 
-W  = '\033[0m'    # white (default)
-R  = '\033[1;31m' # red
-G  = '\033[1;32m' # green bold
-O  = '\033[1;33m' # orange
-B  = '\033[1;34m' # blue
-P  = '\033[1;35m' # purple
-C  = '\033[1;36m' # cyan
-GR = '\033[1;37m' # gray
-
-def banner(judul):
+def banner():
     os.system('clear')
-    print(W)
-    print("  ██╗     ██╗  ██╗██╗  ██╗██████╗  ██╗")
-    print("  ██║     ██║ ██╔╝╚██╗██╔╝╚════██╗███║")
-    print("  ██║     █████╔╝  ╚███╔╝  █████╔╝╚██║")
-    print("  ██║     ██╔═██╗  ██╔██╗ ██╔═══╝  ██║")
-    print("  ███████╗██║  ██╗██╔╝ ██╗███████╗ ██║")
-    print("  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═╝")
-    print(C)
-    if not judul:
-        print("  ════════════════════════════════════")
-    else:
-        print(f"  {judul}")
-    print(W)
+    print('''
 
-if not judul:
-    banner('')
-    judul = input(" Masukkan Judul : ")
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░   ░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓████▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░   ░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░ 
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░ 
+░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░  ░▒▓█▓▒░ 
 
-lk21 = "https://lk21.xn--6frz82g/"
-download = "http://dl.sharemydrive.xyz/"
-# download = "http://dl.indexmovies.xyz/"
+''')
 
-req = requests.get(lk21, params={"s":judul}).text
-soup = BeautifulSoup(req, "html.parser")
-link = soup.find_all('h2')[2]
-try:
-    judul = re.search(r'<a href="'+lk21+'(.*)/" rel="bookmark"', str(link)).group(1)
-except AttributeError:
-    judul = re.search(r'<a href="'+lk21+'(.*)/" rel="bookmark"', str(link))
-try:
-    banner(judul)
-    dload = download + "get/" + judul
-    verif = download + "verifying.php"
-    data = {"Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
-            "Accept":"*/*",
-            "X-Requested-With":"XMLHttpRequest"}
-    req = requests.post(verif, headers=data, params={"slug":judul}).text
-    soup = BeautifulSoup(req, "html.parser")
-    link = soup.find_all('a')
-    if not areso:
-        resolusi = ["360","480","720","1080"]
-    else:
-        resolusi = [areso]
-    for reso in resolusi:
-        ress = re.findall(r'btn-'+reso+'" href="(.*)" rel=', str(link))
-        if len(ress) > 0:
-            for down in ress:
-                if 'cdn' in down:
-                    print(C+"  "+reso+R+" : "+W+down.rsplit('" target', 1)[0])
-                    print(C+"  "+reso+R+" : "+W+down.rsplit('"', 1)[-1])
-                else:
-                    print(C+"  "+reso+R+" : "+W+down)
+def get_links(title, link):
+    print(title)
+    driver = WD.Chrome(service=CS('/usr/bin/chromedriver'), options=options)
+    driver.get(f"https://dl.lk21static.xyz/get/{link}")
+    try:
+        html_content = driver.page_source
+        soup = BS(html_content, "html.parser")
+        items, stream = soup.find_all('tr'), soup.find('p')
+        if stream: tbl.add_row(["Streaming", stream.find('a')['href']])
+        for item in items:
+            provider, link = item.find('strong'), item.find('a')
+            if provider and link: tbl.add_row([provider.text.strip(), link['href']])
+    except Exception as e: print("Gagal menunggu elemen:", e)
+    tbl.align = "l"
+    print(tbl)
+    tbl.clear_rows()
+    driver.quit()
+
+def get_info(title, link):
+    if "Series" in title:
+        res = requests.get(f"https://tv12.nontondrama.click/{link.split('/')[-2]}/", headers={"User-Agent": "Mozilla/5.0"})
+        if res.status_code == 200:
+            soup = BS(res.text, "html.parser")
+            season_list = soup.find_all('div', class_='season-list')
+            for season in season_list:
+                episode_list = season.find_next_sibling('div', class_='episode-list')
+                if episode_list:
+                    episodes = episode_list.find_all('a')
+                    for episode in episodes:
+                        episode_number = episode.text.strip()
+                        if "Info" not in episode_number:
+                            episode_link = episode['href']
+                            get_links(f"{season.find('h4').text.strip()} - Episode {episode_number}", episode_link.split('/')[-2])
+    else: get_links(title, link.split('/')[-2])
+
+if res.status_code == 200:
+    banner()
+    soup = BS(res.text, "html.parser")
+    result = soup.find('div', class_='search-wrapper')
+    items = result.find_all(class_='search-item')
+
+    tbl = PT()
+    tbl.field_names = ["Provider", "Download"]
+    
+    if args.limit:
+        if args.limit == "" or args.limit == None: get_info(items[0].find('h3').text.strip(), items[0].find('a')['href'])
         else:
-            if areso:
-                print(R+"  !! "+W+"Resolusi "+reso+" tidak ditemukan")
-except:
-    print(R+"  !! "+W+"Film tidak ditemukan")
-print ("")
+            i = 0
+            while i < int(args.limit):
+                get_info(items[i].find('h3').text.strip(), items[i].find('a')['href'])
+                i += 1
+    else:
+        for item in items: get_info(item.find('h3').text.strip(), item.find('a')['href'])
+else: print("Gagal melakukan pencarian:", res.status_code)
